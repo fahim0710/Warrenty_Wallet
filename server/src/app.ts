cfd6bd84251a84185f6116c533e838f3f9424956
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-13 17:40:42 +0600

    initialized the server and app

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+    })
+);
+
+app.use(helmet());
+
+app.use(compression());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get("/", (_, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Warranty Wallet API Running",
+    });
+});
+
+export default app;
